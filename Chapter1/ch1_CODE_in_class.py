188fd439622d82337792251a384d7ec1ce9b529a
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-12-08 15:50:42 -0600

    initial commit

---
+++ Chapter1/ch1_CODE_in_class.py
@@ -0,0 +1,81 @@
+### Slide 5 - Simple Hello World Program
+
+
+
+
+###Slide 15 - Data types
+
+
+
+
+
+
+###Slide 17 - Getting Input
+
+
+
+
+
+
+##Slide 18 Activity
+
+
+
+
+
+
+####Slide 19 Recasting Input
+
+
+
+
+
+
+###Slide 22 Outputting multiple data
+
+
+
+pass # Using commas to seperate data
+pass # Using + to concatenate data into one string
+pass # Using f-strings
+
+
+
+###Slide 23 Operations
+
+
+pass # Regular division, always a FLOAT
+pass # Quotient division
+
+
+##Slide 28
+##Run-Time error - Program stops execution when it encounters an error
+# length = int(input("Enter the length: "))
+# print(legnth) # Stops execution HERE
+
+
+##Syntax Error - Program does not execute at all
+# length = int(input("Enter the length: "))
+# print{legnth} # SYNTAX ERROR Here
+
+
+##Sementic Error
+# length = int(input("Enter the length: "))
+# print("The area of the square is ", 2*length)
+
+
+##Slide 29
+ # print("Hello")
+
+
+# a = 2
+# b = 3
+# print(a +)
+
+
+##Slide 30
+##Traceback
+# def say(name):
+#     print('Hello, ' + nam)
+#
+# say('Michael')
