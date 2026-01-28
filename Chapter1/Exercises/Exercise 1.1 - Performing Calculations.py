d87a0e8e636fded792e4ec7573147fa248b8a6be
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-01-28 09:54:30 -0600

    initial commit

---
+++ Chapter1/Exercises/Exercise 1.1 - Performing Calculations.py
@@ -0,0 +1,13 @@
+"""
+Exercise 1.1 - Performing Calculations
+
+Write a Python program that does the following:
+
+    -Asks the user for the base and height of a triangle as an integer.
+    -Calculates and prints the area of the triangle (0.5 * base * height)
+    -Asks the user for the diameter of the circle as a decimal value.
+    -Prints the circumference of the circle (3.14 * diameter)
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
