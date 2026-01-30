5fbe4497123d5aee6ee7b99862c8aae5765f174a
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-01-30 11:05:38 -0600

    initial commit

---
+++ Chapter1/Exercises/Exercise 1.2 - Car Listings.py
@@ -0,0 +1,22 @@
+"""
+Exercise 1.2 - Car Listings
+
+Create a listing for a car. Your program should:
+
+    1)Ask the user for the make/model of the car
+
+    2)Ask the user for the color of the car
+
+    3)Ask the user for the price of the car
+
+    4) Calculate the price of the car after it loses 15% of its value in a year.
+
+    5)Print the car information like the following:
+        “The <color> <car make and model> costs <price>
+
+    6) Print the car information like the following:
+        “Next year the car will be worth <next years price>”
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
