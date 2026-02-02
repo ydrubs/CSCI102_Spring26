cd95ea68462d36e9259a808139fd18f8f9ffd0e0
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-02 10:28:19 -0600

    initial commit

---
+++ Chapter2/ch2_CODE_in_class.py
@@ -0,0 +1,205 @@
+"""
+This is example code for Chapter 2
+This is code we did in class
+"""
+
+##Slide 2 - Example of comments and docstrings
+
+# --- demo of an in-line comment
+# age = 115 # age of the oldest ever living person
+
+
+"""Sometimes there is a need to explain or document a portrion of code beyond one line.
+For this we can use a docstring to extend to multiple lines.
+Docstrings are also used to attribute programs and explain functions."""
+
+# --- Slide 4 - code literals - how data types are represented to the programming language
+
+
+
+
+
+##Slide 5 - place value representation
+
+
+
+
+
+
+##Slide 7 - String literals
+pass # use double quotes of a single quote is part of the string
+
+# --  Often the choice of single or double quote does not matter
+# print("A double quoted string")
+# print('does the same thing as a single quoted string')
+
+# -- Perhaps an exception
+# print("However, there'll be times when a string needs to include a single quote within it, so a double quote is useful.")
+
+
+## Multiline strings look similar to docstrings
+# welcomeText = '''Welcome to my game!
+# I will think of a number from 1 to 100, and you must try
+# to guess it. I'll tell you if your guess is too small
+# or too large.
+
+# Enter an integer between 1 and 100 when prompted.'''
+
+
+# print(welcomeText)
+
+
+##Slide 8 - Escape characters
+# print('Hello. How are you?')
+#
+# print("1. hello " + 'there') #Substitute one of the escape chars for the '#'
+# print("2. hello# " + ' there')
+# print("3. hello# " + ' there')
+# print("4. hello# " + ' there')
+# print("5. hello# " + ' there')
+# print("6. hello#' " + ' there')
+# print("7.hello# " + ' there')
+
+
+
+
+##Slide 10 - End of line arguments
+# print("Hello")
+# print("there")
+
+
+
+
+
+
+##Slide 11 - separator arguments
+# name = 'Yoda'
+# age = 900
+# home_planet = 'Dagobah'
+
+## using sep
+
+
+##  combine sep and end
+
+
+###Activity from slide 13
+## Three valid Variables
+
+
+## Three Invalid variables
+
+
+
+##Slide 14 - variable assignment through concatenation
+# first_name = "Obi-Wan"
+# last_name = "Kenobi"
+
+
+
+
+## Reassigning Variables to new values
+# x = 125
+pass    # y defined in terms of x
+
+
+pass   # redefining x
+
+
+
+pass   # redefining y by modifying
+
+
+
+##Slide 15 - Review Concatenation vs. comma seperation
+# first_name = "Yuriy"
+# last_name = "Drubinskiy"
+
+pass # Concatenation
+pass # comma seperated arguments
+
+
+## Slide 16 - The * string operation
+s1 = 'waaz'
+s2 = 'a'
+s3 = 'p'
+
+
+
+
+
+### ACTIVITY slide 17 - Write a print statement that outputs the string seen in slide 17 of the powerpoint
+
+
+
+##Slide 18 - Examples of math operations
+pass #Exponents
+pass # QUOTIENT
+pass # Modulus (mod)
+pass
+
+pass
+pass
+
+
+
+# a = 1
+pass # Increment and reassign to itself
+
+
+
+
+## Slide 20 Mixed-Mode Arithmetic
+# a = 6
+# b = 3
+pass
+pass # True division Results in a float
+pass # Quotient Division rounds down to an int
+
+
+# c = 0.7
+pass # Addition using at least one float always results in a float
+
+
+
+
+
+#Slide 22 - recasting review
+a = '33'
+b = 5
+c = 4.0
+d = 'one'
+
+pass # int to float
+pass # float to str
+pass # str to int
+pass # str to int - ERROR
+
+
+## SLide 23 rounding floats
+
+
+
+
+
+##Slide 24 - Character operations
+# s1 = "Mary"
+# s2 = "Freddy"
+
+pass #Get the lenght of string s1 and s2
+pass # Get the ASCII code of the character
+pass #Get the character from the ASCII code
+pass #Find the smallest and largest ASCII character of s2
+
+
+##Playing around with some character codes in unicode
+# print(chr(0x265e))
+# print(chr(0x250C))
+# print(chr(203850))
+# print(int(0x265e))
+# print(chr(9822))
+
+# print('The ASCII code for the letter \'a\' is ' + str(ord('a')))
+
+# for i in range (9000, 10000):
+#     print(chr((i)), end = ', ')
