2b02d2ebe88f640f4ecdf32b6aa4afc7b3246f2d
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-06 10:02:48 -0600

    initial commit

---
+++ Chapter2/Exercises/Exercise 2.2 - character encryption.py
@@ -0,0 +1,17 @@
+"""
+Exercise 2.2 - Character Encryption
+
+This is a  simple encryption program that turns a single character into another character
+Do the following:
+
+    1) Ask the user for a single letter (character)
+    2) Ask the user for a number
+    3) Print the character that would result when the input character is shifted by the value in entered
+
+    Example:
+        - User enters 'a'
+        - User enters 5
+        - The result would output 'f'
+
+    See Canvas for more examples
+"""
