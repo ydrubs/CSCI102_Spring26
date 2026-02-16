4fe1592fe544f0f11c2d8333e6ea3729e825ba17
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-16 12:26:00 -0600

    initial commit

---
+++ Chapter3/ch3_CODE_in_class.py
@@ -0,0 +1,254 @@
+##Slide 5 - One way selection statement
+
+
+
+
+
+
+#Slide 6 - If/else statement
+
+
+
+
+
+
+##Slide 7: Multi-way selection
+# number = int(input('Enter the numeric grade: '))
+
+
+
+
+
+## Combining logic with a conditional
+# print("What color is the traffic light?")
+# trafficLight = input("(r)ed, (y)ellow, or (g)reen: ")
+
+
+
+
+
+##Slide 8 ACTIVITY
+
+
+
+##Slide 9
+# x = 10
+# y = 5
+# z = 2
+
+
+
+
+
+##Slide 11
+# a = True
+# b = False
+
+
+
+
+
+# #Write your own challening logic statement, make it as long as you want
+# print(a and not b or b and not a or (a and not b)) # True
+
+
+
+
+
+
+##slide 12 - Evaluating Logic statements
+
+
+
+
+
+
+## Slide 13 Activity
+
+
+
+
+
+##Slide 16 - using a for loop
+
+
+
+
+## using a conditional in a for loop
+
+
+
+
+##Slide 18  - Looping through an algorithm
+
+
+
+
+
+
+
+##Slide 19 - Another example
+
+
+
+
+
+##Slide 20 - Using the loop counter in the loop
+
+
+
+
+##Equivelant to the following but twice as long
+
+
+
+
+##Checkpoint Activity
+"""
+Write a for loop that counts 20 ‘Mississippis’, such as -
+1 Mississippi
+2 Mississippi
+...
+...
+20 Mississippi
+"""""
+
+
+
+
+
+##Slide22 - Controlling the loop range
+
+
+
+
+
+
+##SLide 23 - Controlling the loop range; counting by...
+## Count by threes
+
+
+
+
+## Count backwards from 100
+
+
+
+
+##Slide 24 - Augmented Assignment
+
+
+
+
+
+
+
+### FIZZ BUZZ Exercise (in-class)
+
+
+
+
+
+
+
+
+
+
+##Slide 27 - The while Loop
+""" Ask for a number and add until you hit 1000 """
+
+
+pass # while loop is an entry-controlled loop
+
+
+
+
+
+##slide 29 ACTIVITY
+
+
+
+
+
+
+##Slide 30 - While Loop for entering data
+# theSum = 0.0
+# data = input('Enter a number or just enter to quit: ')
+
+pass # Loop triggers if a string is not empty
+
+
+
+
+##Slide 31 - Breaking a loop
+
+
+
+
+
+
+##Slide 33 - While loop to validate data
+
+pass # Runs forever (until break)
+
+
+
+
+## Slide 34 - The While and the Boolean flag
+
+
+
+
+
+## Slide 35 - Common while loop logic errors
+##  Fail to break loop
+
+# while True:
+#     number = int(input('Enter the numeric grade: '))
+#     if number >= 0 and number <= 100:
+#         print(number)
+#         # NEED TO ADD 'break'
+#
+#     else:
+#         print('Error: grade must be between 100 and 0')
+
+
+################infinite Loop, not updating variable
+# a = 0
+# count = 0
+# while a < 1000:
+#     count += 1
+#     print(a, count)
+
+###############Did not test for a = 500 condition
+# a = 0
+#
+# while a < 1000:
+#     a +=1
+#     if a < 500:
+#         print("Boom", a)
+#     if a > 500:
+#         print('Pow', a)
+    ###Doesn't test a = 500
+
+
+##Slide 36 - Importing Modules -> Random
+
+
+
+
+pass # Don't have to reference random this way
+
+
+
+
+
+
+##Slide 37
+##Dice rolling simulator - generate 10 random numbers between 1 and 6
+
+
+
+
+
+## Guess the number game
