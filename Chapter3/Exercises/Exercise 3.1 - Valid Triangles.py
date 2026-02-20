78b2d178f848aa1861a8bdf1034dfab88f33c70c
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-20 11:10:34 -0600

    initial commit

---
+++ Chapter3/Exercises/Exercise 3.1 - Valid Triangles.py
@@ -0,0 +1,16 @@
+"""
+Exercise 3.1 - Valid Triangles
+
+    The triangle inequality theorem says in order for a triangle to be formed,
+    the sum of any two sides has to be greater than the third.
+    Write a program that accepts the length of three sides as float values
+    and outputs whether it is a valid triangle.
+
+    Remember that it should validate the triangle regardless of which side entered is the longest
+    or if two of the three are equal.
+
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+# YOUR SOLUTION HERE
