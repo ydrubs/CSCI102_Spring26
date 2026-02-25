5e5a37b8d05c80fee9a800f853ef63c2a0f92647
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-25 11:14:15 -0600

    initial commit

---
+++ Chapter3/Exercises/Exercise 3.2 - Sum of Sequences.py
@@ -0,0 +1,33 @@
+
+""""
+Exercise 3.2 - Sum of a sequence
+
+The program asks the user for input and calculates the sum of a sequence based on the following -
+
+    1. Ask the user for the first number in a sequence as a single integer digit.
+    2. Ask the user for the number of terms in a sequence. 
+    
+    Calculate the sum of the sequence - for example:
+        Starting digit is 3
+        Number of terms is 5
+        
+        The program will calculate: 3+33+333+3333+33333
+            ... The program returns 37035 as the answer
+
+    HINT: Think about what you learned in chapter 2 about multiplying strings as well as when to recast to an int.
+
+    Example input:
+        Enter the first number in the sequence (single digit): 1
+        Enter the number of terms: 5
+
+    Example output:
+        The sum of the sequence is: 12345
+
+    Example input:
+        Enter the first number in the sequence (single digit): 6
+        Enter the number of terms: 12
+
+    Example output:
+        The sum of the sequence is: 740740740732
+"""
+# --- YOUR SOLUTION HERE
