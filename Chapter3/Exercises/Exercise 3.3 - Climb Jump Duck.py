013e36e74e2f9823cccd61618e967fe7fa290a78
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-02-27 11:29:52 -0600

    initial commit

---
+++ Chapter3/Exercises/Exercise 3.3 - Climb Jump Duck.py
@@ -0,0 +1,23 @@
+"""
+
+Exercise 3.3 - Climb Jump Duck
+
+In this program, you will practice using a for loop and conditional statements.
+
+    Your task:
+
+    1. Ask the user to enter a positive number.
+    2. Use a for loop to go through all numbers from 1 up to that number.
+    3. For each number, apply the following rules:
+
+       - If the number is divisible by 2, print "Climb"
+       - If the number is divisible by 5, print "Jump"
+       - If the number ends in 3, print "Duck"
+       - If more than one rule applies, print all matching words together
+         (for example: "ClimbJump")
+       - If none of the rules apply, print the number itself.
+
+    Goal:
+        Practice writing for loops, using conditions, and thinking carefully
+        about how different rules can apply to the same number.
+"""
