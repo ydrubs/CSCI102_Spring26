0544b35339b594029769d6cf233bf6f132622e3f
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-27 12:29:36 -0500

    initial commit

---
+++ Chapter4/Exercises/Exercise 4.1 - Password Validation.py
@@ -0,0 +1,22 @@
+"""Password Validation Program
+
+Write a program that asks the user to input a password and validates it. The password is valid as long as:
+1) There is at least 8 characters
+2) There is at least 1 lowercase letter
+3) There is at least one upper case letter
+4) There is at least one digit
+
+If the program is valid, print something that says so, for example “Password has been set!”
+
+If the password is not valid for anyone of the reasons given above the user is asked to re-enter a different password until the conditions are met.
+
+_______________________________________________________________________
+Optional features (Not required for full credit):
+1) Add a fifth condition in which a special character (!,@,#,$,%,^,&,*,(, or ))
+   needs to be included before a password is valid
+
+2) If the password is valid, ask the user to confirm the password by re-entering
+    it before accepting. If the confirmed password is different then the original,
+    the process starts over (asks the user for a password, does validity check,
+    asks for confirmation).
+"""
