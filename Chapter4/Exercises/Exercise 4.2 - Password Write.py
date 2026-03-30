9691d5eeb66a8ad13d478b300386ed19c71816cd
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-03-30 10:44:13 -0500

    initial commit

---
+++ Chapter4/Exercises/Exercise 4.2 - Password Write.py
@@ -0,0 +1,11 @@
+"""
+Extend the function of your password validation program.
+
+Once the password is validated, as done in the previous exercise:
+    – Have Python ask what the password is for then write the password and its application to a text file.
+
+###################################################################################################
+Optional: Ask the user if there are any more passwords to save? If there is, continue asking for,
+          validating, and storing the passwords, until the user says there is no more passwords to save.
+"""
+#YOUR SOLUTION HERE
