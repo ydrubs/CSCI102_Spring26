f9a62f16ca6c1aa8bbde79ab4aa620daeab9579e
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-04-10 09:51:41 -0500

    initial commit

---
+++ Chapter5/Exercises/Exercise 5.1 - Run Times.py
@@ -0,0 +1,14 @@
+"""
+Exercise 5.1 Run Times
+
+Write a program that records a series of run time for a one-mile run into a list using
+a float data type for time such as 7.34.
+
+Your program should continue to ask the users 1-mile time and add the values to the
+list until ‘enter’ is pressed (nothing typed in at the input prompt/blank string)
+
+At that point the program calculates and returns the average 1-mile time from the list of values.
+
+See example Input and output in Canvas for hpw the interaction with the program should be structured
+"""
+#YOUR SOLUTION HERE
