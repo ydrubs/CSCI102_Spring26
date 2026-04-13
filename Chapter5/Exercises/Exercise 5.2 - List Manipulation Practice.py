9f913d570dc78d8c0a3226af6ea7128135459652
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-04-13 09:55:36 -0500

    initial commit

---
+++ Chapter5/Exercises/Exercise 5.2 - List Manipulation Practice.py
@@ -0,0 +1,33 @@
+"""
+Exercise 5.2 List Manipulation Practice
+
+Goal:
+    Manipulate lists using several different list methods to become familiar with how
+    they are used and what they result in
+
+
+START HERE: Copy the starter code given in Canvas or found on Github.
+
+Perform the following operations on the list that was uploaded.
+All list manipulations should use list METHODS to demonstrate understanding
+
+    1) Add Sloth and panda to the list.
+        a. Print this list to the terminal
+
+    2) Create a new list called pets
+
+    3) Remove cat and dog from the previous list
+        a. Print the list to the terminal
+
+    4) Add cat and dog to the pet list.
+
+    5) Between cat and dog in the pets list add cat-dog
+        a. Print this list to the terminal
+
+"""
+import random
+
+lst = ["cat", "dog", "cow", "tiger", "lion", "Fox", "Shark", "Snake", "turtle", "mouse", "monkey", "bear"]
+random.shuffle(lst)
+
+#YOUR CODE HERE
