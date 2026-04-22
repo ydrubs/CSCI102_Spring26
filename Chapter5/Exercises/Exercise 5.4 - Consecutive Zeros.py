b978c6f7527fa3fb0bec1235b45fc8cef59098ed
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-04-22 10:40:09 -0500

    initial commit

---
+++ Chapter5/Exercises/Exercise 5.4 - Consecutive Zeros.py
@@ -0,0 +1,16 @@
+"""
+Exercise 5.4 - Consecutive Zero's
+
+Write a function that accepts a string containing only 1's and 0's.
+The function returns the maximum number of consecutive 0's in the string.
+
+For example if the string is
+    - 1001000010, the function would return four
+    - 110110111, the function would return 1
+    - 1111111, the function would return 0
+
+Note:
+    1) Your string should RETURN the value NOT print it in the function
+    2) Do not use max() in your solution
+"""
+# YOUR SOLUTION HERE
