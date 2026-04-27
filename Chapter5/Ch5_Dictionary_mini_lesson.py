f04f1aad36c0c29f292f427c942395908a7329a6
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-04-27 10:41:01 -0500

    initial commit

---
+++ Chapter5/Ch5_Dictionary_mini_lesson.py
@@ -0,0 +1,74 @@
+"""
+A DICTIONARY is a Python data type that links to pieces of data together inside of curly-brackets {}
+    The first piece of data is called the KEY
+    The second piece of data is called the VALUE
+"""
+
+#Define a dictionary
+pass # Empty Dictionary we can add entries to
+pass # Example dictionary of car makes and models
+
+# We can use the .keys() and the .values() method to view the keys and values separately
+pass # Shows the makes
+pass # Shows the models
+
+
+# We can add to a dictionary by specifying the KEY and setting it equal to a value
+pass
+
+pass # Create a dictionary entry from user input
+
+
+#Dictionary are NOT ORDERED meaning we CAN'T use their INDEX to access them or manipulate them
+pass #This gives an Error because we can't use indexes
+pass # This ALSO gives an error
+pass # This is okay because we reference a key
+
+
+#Create a dictionary using a loop
+pass
+
+
+# Dictionaries KEYS have to be UNIQUE, otherwise, the LAST entry will overwrite the previous similar one
+pass # Replaces another Ford model with F150
+pass
+
+
+
+# We can perform operations on dictionary keys as long as we are using compatible data types:
+# d = {'a': 13, 'b': 7, 'c' : 12}
+pass # Add up all the values
+pass # Error because can't add strings
+
+# --- in-class Practice 1
+"""
+1)Create a dictionary of states and their capital cities that includes:
+          1) Kansas – Topeka 2) Texas – Austin 3) Arkansas – Little Rock
+
+2) Write a line of code that accesses and prints only capital cities in the above dictionary
+2b) Add the state Nebraska and its capital Lincoln
+3) Write a for loop that converts the city in each dictionary all capital letters and print the dictionary
+"""
+pass
+
+""" 4)	Explain - Why does the following cause an error: 
+
+    colors = {'Red': 5, 'Blue': 3}
+    print(colors[0])   # Error
+    
+    ANSWER: ____________________
+
+"""
+
+
+
+# --- in-class Practice 2
+"""
+Given list l1 and list l2 below -
+
+Create a single dictionary where the keys are the elements in list 1 and
+the values are the elements in list 2
+"""
+# l1 = ['a', 'b', 'c']
+# l2 = [1, 2, 3]
+
---
+++ Chapter5/Exercises/Exercise 5.5 - Grocery Dictionary.py
@@ -0,0 +1,11 @@
+"""
+Exercise 5.5 - Grocery Dictionary
+
+1)	Create a dictionary called grocery_cart
+2)	Ask the user for input 3 times. Each time the user should be asked to enter an item then the price of that item.
+3)	Each input should be recorded into the dictionary as a Key-value pair where the key is the item and value is the price.
+4)	Print the dictionary to the terminal
+5)	Use the sum() command to find the total cost of the items (using a dictionary method of course, not direct inputs from step 2)
+
+"""
+#YOUR CODE HERE
